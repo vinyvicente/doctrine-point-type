9bc817e8b3902df49362423bcbbd09c61e0097f3
Daniele Pastori <fullbl@mail.com>
2021-03-22 15:45:09 +0100

    symfony normalizer and denormalizer

---
+++ src/PointNormalizer.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Viny;
+
+use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+class PointNormalizer implements NormalizerInterface, DenormalizerInterface
+{
+    /**
+     * @param Point $object
+     *
+     * @return array{latitude:float,longitude:float}
+     */
+    public function normalize($object, string $format = null, array $context = []): array
+    {
+        return [
+            'latitude' => $object->getLatitude(),
+            'longitude' => $object->getLongitude(),
+        ];
+    }
+
+    public function supportsNormalization($data, string $format = null): bool
+    {
+        return $data instanceof Point;
+    }
+
+    /**
+     * @param array{latitude:float,longitude:float} $data
+     */
+    public function denormalize($data, string $type, string $format = null, array $context = []): Point
+    {
+        return new Point($data['latitude'], $data['longitude']);
+    }
+
+    public function supportsDenormalization($data, string $type, string $format = null): bool
+    {
+        return Point::class === $type;
+    }
+}
